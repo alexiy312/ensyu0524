1a5cb0a5b7b3cbfa10deaeaa231d9fd4bffbd4a6
satoshigp <satoshi3104>
2019-05-24 15:48:41 +0900

    Initial commit

---
+++ ensyu0524/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ensyu0524
+{
+    public partial class Form1 : Form
+    {
+        int vx = -5;
+        int vy = -5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+                vx = -vx;
+            else if (label1.Left > this.Width - 50)  
+                vx=-vx;
+
+            if (label1.Top < 0)
+                vy = -vy;
+            else if (label1.Top > this.Height - 50) 
+                vy = -vy;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //開始時のlabel1の右端、下端の座標を表示する
+            //MessageBox.Show("" + label1.Right);
+            //MessageBox.Show("" + label1.Bottom);
+        }
+    }
+}
